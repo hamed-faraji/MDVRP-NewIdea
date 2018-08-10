4c7957c6ee27edcd4a84a467ef239788d16ff639
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-10 19:01:12 +0430

    Edit Object class Chromosome and Customers

--- Chromosome.java
+++ Chromosome.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
 public class Chromosome {
+	
+ArrayList<Depot> chromosome = new ArrayList<>();
 
 }
--- Customers.java
+++ Customers.java
@@ -1,9 +1,20 @@
 
+import java.util.ArrayList;
 
 public class Customers {
 
-public Customers(int x,int y){
 
-
-}
+		int x;
+		int y;
+		int Cost;
+		boolean depot;
+		int ID;
+		public static ArrayList<Integer> DistanceS = new ArrayList<>();
+		public Customers(int ID,int x,int y,int Cost,boolean depot){
+			this.x = x;
+			this.y = y;
+			this.depot = depot;
+			this.Cost = Cost;
+			this.ID = ID;
+		}
 }
