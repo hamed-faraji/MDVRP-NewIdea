3c0b16727994b4eec06a82722b1f2937996fe355
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-15 18:37:52 +0430

    Add some features to depot object class

--- Depot.java
+++ Depot.java
@@ -1,10 +1,18 @@
 
+import java.util.ArrayList;
 public class Depot {
+	int x;
+    int y;
+    int capacity;
+    int ID;
+    double maxDistance = 0;
+    public  ArrayList<Customers> customers = new ArrayList<>();
+        public Depot(int ID,int x,int y,int capacity){
+        this.x = x;
+        this.y = y;
+        this.capacity = capacity;
+        this.ID = ID;
+      //  this.DistanceS = DistanceS;
 
-public Depot(int x,int y){
-
-
-
-
-}
+      }
 }
