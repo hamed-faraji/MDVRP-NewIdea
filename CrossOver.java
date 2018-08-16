40f711b2cb344ecd79c5a71ee680beeeb2a67c1d
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-16 23:06:22 +0430

    Add some required classes to cross over

---
+++ CrossOver.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class CrossOver {
+	
+	public Chromosome[] crossover(Chromosome chr1 ,Chromosome chr2){
+		
+	}
+	public void makeMatrix(ArrayList<Customers> cus1, ArrayList<Customers> cus2){
+		
+	}
+	public int find(ArrayList<Customers> cus2, Customers cus){
+	
+	}
+	public Depot combine(Depot dp1, Depot dp2){
+	
+	}
+	public Customers getNextCustomer(ArrayList<ArrayList<Index>> matrixTmp, ArrayList<Customers> uniqueCustomersTmp, Customers cus){
+		
+	}
+	public ArrayList<ArrayList<Index>> deleteInMatrix(ArrayList<ArrayList<Index>> matrix, int id){
+	
+	}
+	public ArrayList<ArrayList<Index>> copyMTmp (ArrayList<ArrayList<Index>> asl, ArrayList<ArrayList<Index>> fake){
+	
+	}
+	public ArrayList<Customers> copyUTmp (ArrayList<Customers> asl, ArrayList<Customers> fake){
+	
+	}
+	public boolean check (ArrayList<Customers> cus1, ArrayList<Customers> cus2){
+	
+	}
